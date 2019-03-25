cf9200bcd92b105c10e4876dfc0a96b1789c7e92
mercadier yoann <yoann.mercadier@gmail.com>
2019-03-25 16:15:31 +0100

    test Bootstrap4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { TestcssComponent } from './testcss/testcss.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TestcssComponent],
   imports: [BrowserModule,
   NgbModule
   ],
